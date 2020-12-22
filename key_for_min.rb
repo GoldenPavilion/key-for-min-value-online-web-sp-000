4af8edd3469ccab8d5362d5b02d42d36e0d46c25
Robert Stine <rcorystine@gmail.com>
2020-12-22 01:22:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,10 @@
 def key_for_min_value(name_hash)
   min_key = nil 
   min_value = nil
-  name_hash.collect do |value, num|
+  name_hash.collect do |key, value|
     if min_value == nil || value < min_value 
-      value == m
+      min_value = value 
+      min_key = key
     end
   end 
 end
